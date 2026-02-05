e19fc189a6d3ec90fe2cabcd3ef47be484708dd6
olvibond <bondareva.ov.zakon@gmail.com>
2026-02-05 17:06:52 +0100

    New version Android SDK

---
+++ platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -0,0 +1,25 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+  module.exports = [
+    {
+      "id": "cordova-plugin-splashscreen.SplashScreen",
+      "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+      "pluginId": "cordova-plugin-splashscreen",
+      "clobbers": [
+        "navigator.splashscreen"
+      ]
+    },
+    {
+      "id": "cordova-plugin-statusbar.statusbar",
+      "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+      "pluginId": "cordova-plugin-statusbar",
+      "clobbers": [
+        "window.StatusBar"
+      ]
+    }
+  ];
+  module.exports.metadata = {
+    "cordova-plugin-splashscreen": "6.0.1",
+    "cordova-plugin-whitelist": "1.3.4",
+    "cordova-plugin-statusbar": "4.0.0"
+  };
+});
